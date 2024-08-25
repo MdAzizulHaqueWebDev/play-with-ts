9d5175dd073f63a54a47575ace278757e0d169c8
Md Azizul <khanazizul1199@gmail.com>
2024-08-25 20:09:08 +0600

    first commit

---
+++ src/script.js
@@ -0,0 +1,5 @@
+/** @format */
+console.log("Hello typescript");
+var hi = "hi world";
+hi = "wow it's so easy";
+console.log("hi", hi);
---
+++ src/script.ts
@@ -0,0 +1,6 @@
+/** @format */
+
+console.log("Hello typescript");
+let hi: string = "hi world";
+hi = "wow it's so easy";
+console.log("hi",hi);
